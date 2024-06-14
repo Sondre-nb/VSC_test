043ac80e49ca6300032d72d91fd87ab6d69edd0b
Sondre-nb <sondre.njastad.bjorn@gmail.com>
2024-06-14 16:30:25 +0200

    Added test+number

--- test.py
+++ test.py
@@ -2,3 +2,6 @@ print("Hello World")
 print("This is the second thing I am changing in the code to test stuff")
 print("At this point Git is so slow, im adding stuff to see what will happen")
 print("YouTube video guide test")
+
+for i in range(5):
+    print("Test:" + i)
